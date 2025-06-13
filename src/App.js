efdb7793c2c671e0b1107a4b6e17f96a8602b02b
keerthymohan <keerthymohan19@gmail.com>
2025-06-13 13:11:09 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Addition from './components/Addition';
+import Subtraction from './components/Subtraction';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Addition/>
+      <BrowserRouter>
+      <Routes>
+
+      <Route path='/' element={<Addition/>}/>
+      <Route path='/sub' element={<Subtraction/>}/>
+
+      </Routes>
+      
+      </BrowserRouter>
     </div>
     
   );
--- src/components/Addition.jsx
+++ src/components/Addition.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './Navbar'
 
 const Addition = () => {
 
@@ -23,6 +24,9 @@ const Addition = () => {
 
   return (
     <div>
+
+      <Navbar/>
+
       <div className="container">
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
