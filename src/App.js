a688fe078cb94e560e673ba21e5266c57e3d36ef
keerthymohan <keerthymohan19@gmail.com>
2025-06-13 12:02:31 +0530

    addition

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Addition from './components/Addition';
+
+function App() {
+  return (
+    <div>
+      <Addition/>
+    </div>
+    
+  );
+}
+
+export default App;
